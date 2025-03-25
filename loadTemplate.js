023edc2e35ff75f71d7675eb5bc9ea48456ce785
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-03-25 17:56:57 +0000

    script de carga de templates

---
+++ loadTemplate.js
@@ -0,0 +1,14 @@
+function loadTemplate(fileName, id, callback) {
+
+
+    fetch(fileName).then((res) => {
+        return res.text();
+    }).then((text) => {
+        document.getElementById(id).innerHTML = text;
+
+
+        if(callback){
+            callback();
+        }
+    })
+}
