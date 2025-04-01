4e86e44054da2773b49ce846da09c15b6b5d06d8
Nicogameps5 <nicolashernandezcastro08@gmail.com>
2025-04-01 18:24:27 +0100

    template button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainPageComponent} from './Pages/main-page/main-page.component';
+import {ButtomComponent} from './Templates/buttom/buttom.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainPageComponent, ButtomComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
