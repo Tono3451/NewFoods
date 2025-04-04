08a362f94eeb136e7c0d015b22769e775acab554
Nicogameps5 <nicolashernandezcastro08@gmail.com>
2025-04-04 17:28:11 +0100

    imageTemplate template

---
+++ src/app/Templates/image-template/image-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-template',
+  imports: [],
+  templateUrl: './image-template.component.html',
+  styleUrl: './image-template.component.css'
+})
+export class ImageTemplateComponent {
+
+}
