9a2439f31762195098d5d9032b248e32bf555af0
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-04-04 17:37:38 +0100

    main page component

--- src/app/Pages/main-page/main-page.component.ts
+++ src/app/Pages/main-page/main-page.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../../Templates/header/header.component';
+import {FooterComponent} from '../../Templates/footer/footer.component';
+import {ButtomComponent} from '../../Templates/buttom/buttom.component';
+import {
+  ImageTitleTextHorizontalComponent
+} from '../../Templates/image-title-text-horizontal/image-title-text-horizontal.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ButtomComponent,
+    ImageTitleTextHorizontalComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
