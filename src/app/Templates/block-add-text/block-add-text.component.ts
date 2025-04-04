109698a29e1083d288e545d0e7456b1ecd7ae95d
OmarOrtS <147737596+OmarOrtS@users.noreply.github.com>
2025-04-04 17:15:57 +0100

    block add text

---
+++ src/app/Templates/block-add-text/block-add-text.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block-add-text',
+  imports: [],
+  templateUrl: './block-add-text.component.html',
+  standalone: true,
+  styleUrl: './block-add-text.component.css'
+})
+export class BlockAddTextComponent {
+
+}
