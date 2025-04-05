1bb940c4a3975ace2f8591620846335e6b5a562d
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-04-05 20:57:16 +0100

    pagina del login y modificado el botón

--- src/app/Templates/buttom/buttom.component.ts
+++ src/app/Templates/buttom/buttom.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-buttom',
-  imports: [],
   templateUrl: './buttom.component.html',
-  styleUrl: './buttom.component.css'
+  styleUrls: ['./buttom.component.css']
 })
 export class ButtomComponent {
+  @Input() buttonText: string = 'Lorem';
+  @Input() buttonAction: string = '';
 
+  handleClick(event: Event) {
+    event.preventDefault();
+
+    if (this.buttonAction === 'submit') {
+      const form = (event.target as HTMLElement).closest('form');
+      if (form) {
+        form.requestSubmit();
+      }
+    } else if (this.buttonAction === 'log') {
+      console.log("Button clicked!");
+    }
+  }
 }
