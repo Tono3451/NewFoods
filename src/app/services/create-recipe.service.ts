ff29ca64f82b0e063988a68ac04e3cb0e40e9a05
Nico <jkjkloljbjk@gmail.com>
2025-04-21 13:25:27 +0100

    recipe created in createdRecipes

--- src/app/services/create-recipe.service.ts
+++ src/app/services/create-recipe.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, doc, updateDoc, arrayUnion } from '@angular/fire/firestore';
+import { Auth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecetaService {
 
-  constructor(private firestore: Firestore) {}
+  constructor(private firestore: Firestore, private auth: Auth) {}
 
   async crearReceta(receta: any): Promise<void> {
+    const user = this.auth.currentUser;
+
+    if (!user) {
+      throw new Error('Usuario no autenticado');
+    }
+
     const recetaCollection = collection(this.firestore, 'recipes');
-    await addDoc(recetaCollection, receta);
+    const recetaDocRef = await addDoc(recetaCollection, {
+      ...receta,
+      creatorId: user.uid // Guardamos el UID en la receta por referencia cruzada
+    });
+
+    const userRef = doc(this.firestore, 'users', user.uid);
+    await updateDoc(userRef, {
+      createdRecipes: arrayUnion(recetaDocRef.id)
+    });
   }
 }
