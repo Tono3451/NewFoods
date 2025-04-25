a223e2dc746ca85cc7128ab9a9f3ccf85c825d7b
Omar <omar.ortega102@alu.ulpgc.es>
2025-04-25 17:51:52 +0100

    saved and likes linked to firestore

--- src/app/Templates/image-title-text-horizontal/image-title-text-horizontal.component.ts
+++ src/app/Templates/image-title-text-horizontal/image-title-text-horizontal.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import {Recipe, RecipeService} from '../../services/recipe.service';
 import { Router } from '@angular/router';
 import {Auth, onAuthStateChanged, User} from '@angular/fire/auth';
+import {UserService} from '../../services/user.service';
 
 @Component({
   selector: 'app-image-title-text-horizontal',
@@ -14,7 +15,7 @@ export class ImageTitleTextHorizontalComponent {
   userId: string = '';
 
   constructor(private router: Router, private recipeService: RecipeService,
-              private auth: Auth) {}
+              private auth: Auth, private userService: UserService,) {}
 
   ngOnInit() {
     onAuthStateChanged(this.auth, (user: User | null) => {
@@ -39,5 +40,11 @@ export class ImageTitleTextHorizontalComponent {
     if (!this.userId || !this.recipe?.id) return;
     const updated = await this.recipeService.toggleSave(this.recipe.id, this.userId);
     this.recipe.saved = updated.saved;
+
+    if (updated.savedBy.includes(this.userId)) {
+      await this.userService.addRecipeToSaved(this.userId, this.recipe.id);
+    } else {
+      await this.userService.removeRecipeFromSaved(this.userId, this.recipe.id);
+    }
   }
 }
