939be9c4a8cc14c75e0f93fc91984741ef7554b3
Omar <omar.ortega102@alu.ulpgc.es>
2025-04-25 17:44:33 +0100

    like and svaed button functional

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {Auth, authState, User} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  user$: Observable<User | null>;
+
+  constructor(private auth: Auth) {
+    this.user$ = authState(this.auth);
+  }
 
   isLoggedIn(): boolean {
     return !!localStorage.getItem('token');
