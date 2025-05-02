3e5fc824bb120c0cd18b860b92781ed13ce97e89
Tono3451 <116306863+Tono3451@users.noreply.github.com>
2025-05-02 18:32:41 +0100

    mostrar recetas en el usuario

--- src/app/Pages/profile-page/profile-page.component.ts
+++ src/app/Pages/profile-page/profile-page.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../Templates/header/header.component';
 import { FooterComponent } from '../../Templates/footer/footer.component';
 import { AvatarComponent } from '../../Templates/avatar/avatar.component';
-import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
 import { LoginServiceService } from '../../services/login-service.service';
 import {onAuthStateChanged} from '@angular/fire/auth';
+import {RecipeService} from '../../services/recipe.service';
+import {
+  ImageTitleTextHorizontalComponent
+} from '../../Templates/image-title-text-horizontal/image-title-text-horizontal.component';
 
 @Component({
   selector: 'app-profile-page',
@@ -15,18 +18,23 @@ import {onAuthStateChanged} from '@angular/fire/auth';
     HeaderComponent,
     FooterComponent,
     AvatarComponent,
-    RouterLink,
-    CommonModule
+    CommonModule,
+    ImageTitleTextHorizontalComponent
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.css'
 })
 export class ProfilePageComponent implements OnInit {
   nombre: string = 'Usuario';
+  recipesCreated:   any[] = [];
+  recipesSaved:   any[] = [];
+
+  showSavedRecipesFlag: boolean = true;
 
   constructor(
     private userService: UserService,
-    private loginServiceService: LoginServiceService
+    private loginServiceService: LoginServiceService,
+    private recipeService: RecipeService
   ) {}
 
   ngOnInit() {
@@ -38,6 +46,9 @@ export class ProfilePageComponent implements OnInit {
 
           this.nombre = userData.nombre;
 
+          await this.getUserCreatedRecipes(userData.createdRecipes);
+          await this.getUserSavedRecipes(userData.savedRecipes);
+
         } catch (error) {
           console.error('Error al obtener datos del usuario:', error);
         }
@@ -45,5 +56,42 @@ export class ProfilePageComponent implements OnInit {
         console.log('No hay usuario autenticado.');
       }
     });
+  }
+
+
+  async getUserCreatedRecipes(recipeUIDs: string[]) {
+    try {
+      for (let i = 0; i < recipeUIDs.length; i++) {
+        // Usa await para esperar a que la promesa se resuelva
+        const recipe = await this.recipeService.getRecipeById(recipeUIDs[i]);
+
+        // Ahora puedes acceder a las propiedades del objeto recipe
+        this.recipesCreated.push(recipe);
+      }
+    } catch (error) {
+      console.error('Error al obtener recetas:', error);
+    }
+  }
+
+  async getUserSavedRecipes(recipeUIDs: string[]) {
+    try {
+      for (let i = 0; i < recipeUIDs.length; i++) {
+        // Usa await para esperar a que la promesa se resuelva
+        const recipe = await this.recipeService.getRecipeById(recipeUIDs[i]);
+
+        // Ahora puedes acceder a las propiedades del objeto recipe
+        this.recipesSaved.push(recipe);
+      }
+    } catch (error) {
+      console.error('Error al obtener recetas:', error);
+    }
+  }
+
+  showSavedRecipes() {
+    this.showSavedRecipesFlag = true;
+  }
+
+  showCreatedRecipes() {
+    this.showSavedRecipesFlag = false;
   }
 }
